758e682a118ac644cf823dd7764dea79724c4f11
Evan Bolyen <ebolyen@gmail.com>
2023-05-10 15:23:14 -0700

    fix distro detection

--- .github/workflows/bin/determine.py
+++ .github/workflows/bin/determine.py
@@ -41,7 +41,7 @@ def identify_distro(changed_files):
             continue
 
         base = segments[0]
-        if base.startswith('2') and '.' in base and segments[2] == 'passed':
+        if base.startswith('2') and '.' in base and segments[2] != 'staged':
             distros.add(segments[1])
 
     if len(distros) > 1:
